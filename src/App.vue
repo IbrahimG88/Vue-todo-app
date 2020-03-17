614c18cc38a3d78c6e59b2d404a25d78f3f94984
IbrahimG88 <goharyibrahim@gmail.com>
2020-03-17 21:30:05 +0200

    Second commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos = "todos"/>
+    
+  </div>
+</template>
+
+<script>
+import Todos from './components/Todos';
+
+export default {
+  name: 'App',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      //source of the todos prop: notice next line
+      todos:[{
+        id:1,
+        title:"Todo one",
+        completed: false
+      },
+      {
+        id:2,
+        title:"Todo two",
+        completed: true
+      },
+      {
+        id:3,
+        title:"Todo three",
+        completed: false
+      }]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
