ea09c85b45a07e952208225615d21c1708a90f34
IbrahimG88 <goharyibrahim@gmail.com>
2020-03-20 11:56:50 +0200

    is completed functionality, TodoItem component

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,34 @@
+<template>
+    <!-- add a conditional for completed true or false -->
+    <div class="todo-item" v-bind:class="{'is-complete': todo.completed}">
+        <p>
+        <!-- v-on adds an event, variations: change, submit, click, accepts a method -->
+        <input type="checkbox" v-on:change="markComplete"/>
+        {{todo.title}}
+        <!-- when clicked emit an event called 'del-todo', that includes the value of todo.id -->
+        <button class="del" @click="$emit('del-todo', todo.id)">x</button>
+        </p>
+    </div>
+</template>
+    
+<script>
+    export default {
+        name: "TodoItem",
+        props:["todo"], 
+        //object of functions
+        methods: {
+            markComplete() {
+                //console.log(123)
+                // invert using the not operator the value of comleted
+                this.todo.completed = !this.todo.completed;
+            }
+            }
+
+    }
+</script>
+
+<style>
+.is-complete {
+    text-decoration: line-through;
+}
+</style>
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,15 +1,21 @@
 <template>
 <div>
-<div v-bind:key="todo.id" v-for ="todo in todos">
+<div v-bind:key="todo.id" v-for="todo in todos">
     <h1>Todos</h1>
     <h3>{{ todo.title }}</h3>
+    <!-- v-bind adds a prop, here it is using the todo from the for loop   -->
+    <TodoItem v-bind:todo="todo" />
 </div>
 </div>
 </template>
 
 <script>
+import TodoItem from './TodoItem.vue';
 export default {
     name: 'Todos',
+    components:{
+        TodoItem
+    },
     props:["todos"]
 }
 </script>
